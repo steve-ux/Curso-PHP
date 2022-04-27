941085af6a2eebaf7893f7d37b3e83e3f55a130e
steve-ux <exe.universal@gmail.com>
2022-04-27 17:29:02 -0300

    Ejercitación con SQL

--- database/seeds/ProfessionSeeder.php
+++ database/seeds/ProfessionSeeder.php
@@ -13,7 +13,26 @@ class ProfessionSeeder extends Seeder
     public function run()
     {
 
-        DB::table()->insert('INSERT INTO profession (title) VALUES ("Desarrollador back-end")');
+        DB::insert('INSERT INTO profession (title) VALUES (:title)', [
+            'title' => 'Desarrollador back-end',
+        ]);
+
+        DB::insert('INSERT INTO profession (title) VALUES (:title)', [
+            'title' => 'Desarrollador front-end',
+        ]);
+
+        DB::insert('INSERT INTO profession (title) VALUES (:title)', [
+            'title' => 'Web designer',
+        ]);
+
+        DB::insert('INSERT INTO profession (title) VALUES (:title)', [
+            'title' => 'Fullstack developer',
+        ]);
+
+        DB::delete('DELETE FROM profession WHERE (title) = (:title)',[
+            'title' => 'Fullstack developer',
+        ]);
+
 
         // DB::table('profession')->insert([
         //     'title' => 'Desarrollador back-end',
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,10 +12,26 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        //$profession = DB::select('SELECT id FROM profession LIMIT 0,1', ['Desarrollador back-end']);
+
+        $professionId = DB::table('profession')
+        ->where('title', 'Web designer')
+        ->value('id');
+
+        // $profession = DB::table('profession')->select('id');
+
         DB::table('users')->insert([
             'name' => 'Steve',
             'email' => 'esteban@zaldivar.com',
             'password' => bcrypt('12345'),
+            'profession_id' => $professionId,
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Luke',
+            'email' => 'luke@zaldivar.com',
+            'password' => bcrypt('123456'),
+            'profession_id' => $professionId,
         ]);
     }
 }
